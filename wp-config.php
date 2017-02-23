3dbfece5046ec279cc66dd3758cf994847a72b85
ww930\a622880 <bhushan.rade@atos.net>
2017-02-23 11:58:30 +0530

    commit

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'sampledb');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
 define('DB_HOST', 'sampledb.cnl6x87rqwuo.us-west-2.rds.amazonaws.com:3306');
