1c57a2fae917dd0b774106255ca3bb42b8a33ae1
bhushanrade <bhushanrade123@gmail.com>
2017-04-06 11:58:32 +0530

    commit

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_NAME', 'sampledb');
 
 /** MySQL database username */
-define('DB_USER', getenv('DB_USER'));
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', getenv('DB_HOST'));
+define('DB_HOST', 'sampledb.cnl6x87rqwuo.us-west-2.rds.amazonaws.com:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
